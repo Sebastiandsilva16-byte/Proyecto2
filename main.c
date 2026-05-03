43bdd8d40377327158189cdaf01e9425c997f3b4
Sebastiandsilva16-byte <sebastiandsilva16@gmail.com>
2026-05-02 22:17:27 -0600

    Initial commit

---
+++ main.c
@@ -0,0 +1,36 @@
+/*
+ * Proyecto
+ *
+ * Author: Sebastian Da Silva 
+ */
+// ====================================================================
+// Libraries
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <stdlib.h>
+// ====================================================================
+// Variables
+
+// ====================================================================
+// Function prototypes
+
+// ====================================================================
+// Main Function
+int main(void) {
+	setup();
+	while(1) {
+		
+	}
+}
+// ====================================================================
+// NON-Interrupt subroutines
+
+void setup(void) {
+}
+
+
+
+// ====================================================================
+// Interrupt subroutines
